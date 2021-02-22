4e0f2bd3b519b51816051f31368b23ae462b085e
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-02-22 16:35:52 -0300

    Rotas de exemplo
    Criando algumas rotas apenas para exemplificar como ela funciona

--- api/src/server.js
+++ api/src/server.js
@@ -2,4 +2,15 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3333, ()=> console.log("Server is running!"))
+/* Rotas
+- GET: Buscar
+- POST: Salvar
+- PUT: Alterar
+- DELETE: Deletar
+- PATCH: Alteração específica
+*/
+
+app.get('/', (req, res) => res.send('Hello World - NLW04'))
+app.post('/', (req, res) => res.send('Os dados forma salvos com sucesso'))
+
+app.listen(3333, () => console.log('Server is running!'))
