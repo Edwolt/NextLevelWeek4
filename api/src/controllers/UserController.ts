095d4893acdb6f4e07b4958ee281d8d1fac1e856
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-02-23 15:43:24 -0300

    Terminando lógica do User

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import User from '../models/User'
 
 class UserController {
     async create(req: Request, res: Response) {
-        const body = req.body
-        console.log(body)
-        return res.send()
+        const { name, email } = req.body
+
+        const repository = getRepository(User)
+
+        const exists = await repository.findOne({ email }) // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        if (exists) return res.status(400).json({ error: 'User already exists!' })
+
+        const user = repository.create({ name, email })
+
+        await repository.save(user)
+        return res.json(user)
     }
 }
 
