0b97ce5284dfc26c4d1239c897a04f0ffa035399
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-02-25 11:27:57 -0300

    Criando um teste de exemplo

---
+++ api/src/__tests__/First.test.ts
@@ -0,0 +1,5 @@
+describe('First', () => {
+    it('deve ser possivel somar 2 números', () => { expect(2 + 2).toBe(4) })
+    it('deve ser possivel somar 2 números', () => { expect(2 + 2).toBe(5) })
+    it('deve ser possivel somar 2 números', () => { expect(2 + 2).not.toBe(5) })
+})
