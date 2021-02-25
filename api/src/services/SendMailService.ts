6909aa9d8e975685dfdec6a6d808503fa26637e1
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-02-25 15:41:31 -0300

    Usando o template para enviar email

--- api/src/services/SendMailService.ts
+++ api/src/services/SendMailService.ts
@@ -1,4 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer'
+import { resolve } from 'path'
+import handlebars from 'handlebars'
+import fs from 'fs'
 
 class SendMailService {
     private client: Transporter
@@ -16,7 +19,13 @@ class SendMailService {
     }
 
     async execute(to: string, subject: string, body: string) {
-        const message = await this.client.sendMail({ to, subject, html: body, from: 'NPS <noreplay@nps.com.br>' })
+        const templatePath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
+        const templateFile = fs.readFileSync(templatePath).toString('utf-8')
+
+        const templateParse = handlebars.compile(templateFile)
+        const html = templateParse({ name: to, title: subject, description: body })
+
+        const message = await this.client.sendMail({ to, subject, html, from: 'NPS <noreplay@nps.com.br>' })
         console.log('Message sent: ' + message.messageId)
         console.log('Preview URL ' + nodemailer.getTestMessageUrl(message))
     }
