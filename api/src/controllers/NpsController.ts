4698fb03fbc221f0a1c6d53ffefa5bdc18488f1a
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-02-26 23:29:55 -0300

    Removendo do calculo do NPS quem não respondeu
    Removendo do calculo do NPS o quem ainda não respondeu a pesquisa, e
    portanto tem a nota como NULL

--- api/src/controllers/NpsController.ts
+++ api/src/controllers/NpsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, IsNull, Not } from "typeorm";
 import SurveysUsersRepository from "../repositories/SurveysUsersRepository";
 
 export default class NpsController {
@@ -15,12 +15,10 @@ export default class NpsController {
         */
 
         const survey_id = req.params.survey_id
-        console.log(survey_id)
 
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
 
-        const surveysUsers = await surveysUsersRepository.find({ survey_id })
-        console.log(surveysUsers)
+        const surveysUsers = await surveysUsersRepository.find({ survey_id, value: Not(IsNull()) })
 
         const detractors = surveysUsers.filter(survey => survey.value >= 0 && survey.value <= 6).length
         const promoters = surveysUsers.filter(survey => 9 && survey.value <= 10).length
